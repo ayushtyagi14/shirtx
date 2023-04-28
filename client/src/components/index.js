e8bf57cc7285609cc12c6f39e82fe16475612666
Ayush Tyagi <ayush.tyagi145@gmail.com>
2023-04-28 12:50:39 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import FilePicker from './FilePicker';
+import Tab from './Tab';
+import AIPicker from './AIPicker'
+import ColorPicker from './ColorPicker'
+
+export { CustomButton, FilePicker, Tab, AIPicker, ColorPicker };
